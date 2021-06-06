807bf10cdb8680237df0a88e07788135846fac9b
João Pedro Lescano Morisson <joaopedro@jeraapps.com>
2021-06-05 21:46:11 -0400

    back pesquisa

---
+++ app/controllers/finds_controller.rb
@@ -0,0 +1,12 @@
+class FindsController < ApplicationController
+    def index
+        @movie_name = params[:movie_name]
+
+        @search = Tmdb::Search.new
+        @search.resource('person') # determines type of resource
+        @search.query(@movie_name) # the query to search against
+        
+        @movie_finds = @search.fetch # makes request
+    end
+
+end
