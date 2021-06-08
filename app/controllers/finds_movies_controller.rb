0cf5605b3edcf44087e47a45bfda5317582c9458
João Pedro Lescano Morisson <joaopedro@jeraapps.com>
2021-06-08 03:04:28 -0400

    testando coisas novas

---
+++ app/controllers/finds_movies_controller.rb
@@ -0,0 +1,28 @@
+class find_moviesController < ApplicationController
+    def index
+        finds_movies = current_user.finds_movies
+        @findmovies = []
+
+        finds_movies.each do |find_movies|
+            movie = Tmdb::Movie.detail(finds_movies.movie_finds)
+            @findsmovies.push(movie)
+        end
+
+        @findsmovies
+
+    end
+
+    def create
+        puts params[:movie_finds]
+        current_user.finds_movies.create(movie_finds: params [:movie_finds])
+
+        redirect_to finds_movies_path
+
+    end
+
+    def destroy
+        current_user.finds_movies.find_by(movie_finds: params [:id]).destroy
+
+        redirect_to finds_movies_path
+    end
+end
