31778d6033fbc7dcd917d5c0562091b8e0ba412d
João Pedro Lescano <joaopedro@jeraapps.com>
2021-06-10 21:56:36 -0400

    aplicação pronta

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,10 @@
 
 ActiveRecord::Schema.define(version: 2021_06_08_172843) do
 
-  create_table "finded_movies", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "finded_movies", force: :cascade do |t|
     t.integer "movie_code"
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
@@ -20,7 +23,7 @@ ActiveRecord::Schema.define(version: 2021_06_08_172843) do
     t.index ["user_id"], name: "index_finded_movies_on_user_id"
   end
 
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "users", force: :cascade do |t|
     t.string "name"
     t.date "birthdate"
     t.string "email", default: "", null: false
